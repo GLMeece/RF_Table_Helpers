98ce14b1ed7fb8729f26e5705910b15ab4928275
Greg Meece <glmeece@gmail.com>
2015-05-31 23:29:34 -0500

    Create rf_Table_Helpers.py
    Initial commit. Just the header. I may or may not use lxml this go-round, but I've declared it just in case.

---
+++ rf_Table_Helpers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. module:: rf_Table_Helpers
+   :platform: Unix, Windows
+   :synopsis: Useful table introspection functions for Robot Framework's Selenium 2 Library.
+
+.. moduleauthor:: Greg Meece <glmeece@gmail.com>
+
+"""
+
+from lxml import html
+
+# ---------------------------------------------------------------
